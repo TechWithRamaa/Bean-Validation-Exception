3f39df063d105afef95deca6a6029a129b665a23
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-05-15 21:53:01 +0530

    Spring Boot App In progress

---
+++ src/main/java/com/engineeringwithramaa/beanvalidationandexceptionhandling/BeanvalidationandexceptionhandlingApplication.java
@@ -0,0 +1,13 @@
+package com.engineeringwithramaa.beanvalidationandexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanvalidationandexceptionhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanvalidationandexceptionhandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engineeringwithramaa/beanvalidationandexceptionhandling/BeanvalidationandexceptionhandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineeringwithramaa.beanvalidationandexceptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanvalidationandexceptionhandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
