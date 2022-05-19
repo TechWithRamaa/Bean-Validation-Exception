f3edf63f9846d850f83078ac001b7f54c8a4d20e
EngineeringWithRamaa <rkramadhevi@gmail.com>
2022-05-19 20:18:26 +0530

    application.yml - h2 configurations updated

--- src/main/java/com/engineeringwithramaa/beanvalidationandexceptionhandling/BeanvalidationandexceptionhandlingApplication.java
+++ src/main/java/com/engineeringwithramaa/beanvalidationandexceptionhandling/BeanvalidationandexceptionhandlingApplication.java
@@ -2,8 +2,10 @@ package com.engineeringwithramaa.beanvalidationandexceptionhandling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.engineeringwithramaa.beanvalidationandexceptionhandling")
 public class BeanvalidationandexceptionhandlingApplication {
 
     public static void main(String[] args) {
